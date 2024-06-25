ff509a85a0f6929bd50667ea8524c15b9953ca4c
kumar <kumar420gaurav@gmail.com>
2024-06-25 13:10:02 +0530

    spring boot demo 2

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,15 +1,27 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
+//@EnableWebMvc
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping(value="/login", method= RequestMethod.GET)
     @ResponseBody
-    public String login() {
-        return "Hello Kumar! All is well. You are a genius! jbjh";
+    public String login(@RequestParam String thing) {
+        return thing + " You are a genius!";
+    }
+
+    @RequestMapping("/jsp/login")
+    public String jspLogin(@RequestParam String thing, ModelMap modelMap) {
+        // passing request param to Model which will be available in view
+        modelMap.put("thing", thing);
+        return "login";
     }
 }
